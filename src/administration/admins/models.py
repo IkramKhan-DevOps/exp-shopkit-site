06926fd9bf507f1ab11c5603fa38c637b909a811
Ikram Khan <ikram.khan0762@gmail.com>
2023-03-04 01:53:10 +0500

    INIT: admins app initialized

---
+++ src/administration/admins/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Application(models.Model):
+    pass
+
+
+class ApplicationRegistration(models.Model):
+    pass
+
+
+class DemoRequest(models.Model):
+    pass
+
+
+class ComplainRequest(models.Model):
+    pass
